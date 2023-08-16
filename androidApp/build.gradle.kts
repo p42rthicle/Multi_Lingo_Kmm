15edbd1d77ee5cbf30f950b4fb87f389c17d137c
patthhar <parthdroidworld@gmail.com>
2023-08-16 19:48:29 +0530

    Dependency Management + Added dependencies reqd

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,49 +1,63 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+  id("com.android.application")
+  kotlin("android")
+  id("kotlin-kapt")
+  id("dagger.hilt.android.plugin")
+  kotlin("plugin.serialization") version Deps.kotlinVersion
 }
 
 android {
-    namespace = "me.darthwithap.kmm.multilingo.android"
-    compileSdk = 33
-    defaultConfig {
-        applicationId = "me.darthwithap.kmm.multilingo.android"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+  namespace = "me.darthwithap.kmm.multilingo.android"
+  compileSdk = 33
+  defaultConfig {
+    applicationId = "me.darthwithap.kmm.multilingo.android"
+    minSdk = 24
+    targetSdk = 33
+    versionCode = 1
+    versionName = "1.0"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = Deps.composeVersion
+  }
+  packagingOptions {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-    buildFeatures {
-        compose = true
+  }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  }
 }
 
 dependencies {
-    implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.activity:activity-compose:1.7.1")
+  implementation(project(":shared"))
+  implementation(Deps.composeUi)
+  implementation(Deps.composeUiTooling)
+  implementation(Deps.composeUiToolingPreview)
+  implementation(Deps.composeFoundation)
+  implementation(Deps.composeMaterial)
+  implementation(Deps.activityCompose)
+  implementation(Deps.composeIconsExtended)
+  implementation(Deps.composeNavigation)
+  implementation(Deps.coilCompose)
+
+  implementation(Deps.hiltAndroid)
+  kapt(Deps.hiltAndroidCompiler)
+  kapt(Deps.hiltCompiler)
+  implementation(Deps.hiltNavigationCompose)
+
+  implementation(Deps.ktorAndroid)
+
+  androidTestImplementation(Deps.testRunner)
+  androidTestImplementation(Deps.jUnit)
+  androidTestImplementation(Deps.composeTesting)
+  debugImplementation(Deps.composeTestManifest)
+
+  kaptAndroidTest(Deps.hiltAndroidCompiler)
+  androidTestImplementation(Deps.hiltTesting)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,24 @@
-plugins {
-    //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.2.0-alpha09").apply(false)
-    id("com.android.library").version("8.2.0-alpha09").apply(false)
-    kotlin("android").version("1.8.21").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+buildscript {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath(Deps.kotlinGradlePlugin)
+    classpath(Deps.androidBuildTools)
+    classpath(Deps.sqlDelightGradlePlugin)
+    classpath(Deps.hiltGradlePlugin)
+  }
+}
+
+allprojects {
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+  delete(rootProject.buildDir)
 }
