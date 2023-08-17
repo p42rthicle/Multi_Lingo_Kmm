a06693f18963371b74a02e6039f11a3794b39204
patthhar <parthdroidworld@gmail.com>
2023-08-17 14:29:53 +0530

    An enum class to handle Exception and errors thrown by the Api

---
+++ shared/src/commonMain/kotlin/me/darthwithap/kmm/multilingo/translate/domain/translate/TranslateError.kt
@@ -0,0 +1,11 @@
+package me.darthwithap.kmm.multilingo.translate.domain.translate
+
+enum class TranslateError {
+  ServiceUnavailable,
+  ClientError,
+  ServerError,
+  UnknownError
+}
+
+class TranslateException(error: TranslateError) :
+  Exception("Invalid or unsupported language. Error: $error")
