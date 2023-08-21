f08188223457a5ca138c24e6481d0f98461ca980
patthhar <parthdroidworld@gmail.com>
2023-08-21 13:58:09 +0530

    Routes and navigation setup for app

--- androidApp/src/main/java/me/darthwithap/kmm/multilingo/android/MainActivity.kt
+++ androidApp/src/main/java/me/darthwithap/kmm/multilingo/android/MainActivity.kt
@@ -6,8 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import me.darthwithap.kmm.multilingo.android.core.presentation.Routes
+import me.darthwithap.kmm.multilingo.android.translate.presentation.AndroidTranslateViewModel
+import me.darthwithap.kmm.multilingo.android.translate.presentation.TranslateScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -17,8 +29,21 @@ class MainActivity : ComponentActivity() {
           modifier = Modifier.fillMaxSize(),
           color = MaterialTheme.colors.background
         ) {
+          TranslateRoot()
         }
       }
+    }
+  }
+}
+
+@Composable
+fun TranslateRoot() {
+  val navController = rememberNavController()
+  NavHost(navController = navController, startDestination = Routes.Translate) {
+    composable(route = Routes.Translate) {
+      val viewModel: AndroidTranslateViewModel = hiltViewModel()
+      val state by viewModel.state.collectAsState()
+      TranslateScreen(state = state, onEvent = viewModel::onEvent)
     }
   }
 }
---
+++ androidApp/src/main/java/me/darthwithap/kmm/multilingo/android/core/presentation/Routes.kt
@@ -0,0 +1,6 @@
+package me.darthwithap.kmm.multilingo.android.core.presentation
+
+object Routes {
+  const val Translate = "translate"
+  const val VoiceToText = "voice_to_text"
+}
