c7343401a45e4874dd0b0a8349c973565f716510
patthhar <parthdroidworld@gmail.com>
2023-08-21 14:20:20 +0530

    Added LanguageIconDisplay + Animated Progress Button

---
+++ androidApp/src/main/java/me/darthwithap/kmm/multilingo/android/translate/presentation/components/LanguageIconDisplay.kt
@@ -0,0 +1,27 @@
+package me.darthwithap.kmm.multilingo.android.translate.presentation.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import me.darthwithap.kmm.multilingo.android.core.theme.LightBlue
+import me.darthwithap.kmm.multilingo.core.presentation.UiLanguage
+
+@Composable
+fun LanguageIconDisplay(
+  modifier: Modifier,
+  language: UiLanguage
+) {
+  Row(
+    modifier = modifier,
+    verticalAlignment = Alignment.CenterVertically
+  ) {
+    SmallLanguageIcon(modifier = modifier, language = language)
+    Spacer(modifier = Modifier.width(8.dp))
+    Text(text = language.language.languageName, color = LightBlue)
+  }
+}
---
+++ androidApp/src/main/java/me/darthwithap/kmm/multilingo/android/translate/presentation/components/SmallLangugaeIcon.kt
@@ -0,0 +1,20 @@
+package me.darthwithap.kmm.multilingo.android.translate.presentation.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import me.darthwithap.kmm.multilingo.core.presentation.UiLanguage
+
+@Composable
+fun SmallLanguageIcon(
+  modifier: Modifier,
+  language: UiLanguage
+) {
+  AsyncImage(
+    modifier = modifier.size(24.dp),
+    model = language.drawableRes,
+    contentDescription = language.language.languageName
+  )
+}
