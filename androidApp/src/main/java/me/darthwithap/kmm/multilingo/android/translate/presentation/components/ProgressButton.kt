f6c96a5725d5bbf4b1ba5a8255200994031162e7
patthhar <parthdroidworld@gmail.com>
2023-08-23 13:59:56 +0530

    Changed font weight on translate button to semi_bold on android

--- androidApp/src/main/java/me/darthwithap/kmm/multilingo/android/translate/presentation/components/ProgressButton.kt
+++ androidApp/src/main/java/me/darthwithap/kmm/multilingo/android/translate/presentation/components/ProgressButton.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -42,7 +43,8 @@ fun ProgressButton(
       } else {
         Text(
           text = text.uppercase(),
-          color = MaterialTheme.colors.onPrimary
+          color = MaterialTheme.colors.onPrimary,
+          fontWeight = FontWeight.SemiBold
         )
       }
 
